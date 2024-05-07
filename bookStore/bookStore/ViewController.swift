7a14f8bebbc6ced412802b799242b1bcf4a6c2d5
Nayeon Chae <cny1230@naver.com>
2024-05-07 11:18:40 +0900

    files changed

---
+++ bookStore/bookStore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  bookstore
+//
+//  Created by 채나연 on 5/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.purple
+    }
+
+
+}
+
